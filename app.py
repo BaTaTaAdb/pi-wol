39de62f6ea394f114e9d20874573c8bb0a37ab58
BaTaTaAdb <joaominecraft.adr@gmail.com>
2023-01-22 20:13:30 +0000

    added test n2

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import pifacedigitalio as pfio
 from time import sleep
 
-pfio.init()
-pfio.digital_write(0,0)
+
 
 
 def turn_on():
+    pfio.init()
+    pfio.digital_write(0,0)
     pfio.digital_write(0,1)
     sleep(0.1)
     pfio.digital_write(0,0)
--- mqtt_publish_turn_on.py
+++ mqtt_publish_turn_on.py
@@ -6,6 +6,6 @@ from time import sleep
 
 publish.single("BaTaTaAdb/test", "turn_on", hostname="test.mosquitto.org")
 sleep(5)
-publish.single("BaTaTaAdb/pc", "turn_off!", hostname="test.mosquitto.org")
+publish.single("BaTaTaAdb/pc", "turn_off", hostname="test.mosquitto.org")
 print("Done")
  
