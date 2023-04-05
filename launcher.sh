c8dd1d095fcebd1200a7e85a57390e12e4a58cf8
João Nuno <joaominecraft.adr@gmail.com>
2023-04-06 00:03:46 +0100

    changed launcher to activate on internet

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,23 @@
-#!/bin/sh
-# launcher.sh
-# navigate to pi-wol, then starts the client
-cd /
-cd /home/pi/pi-wol
-sudo python3 mqtt_daemon.py
-cd /
+#!/bin/bash
+((count = 60)) # Maximum number to try.
+while [[ $count -ne 0 ]]; do
+    ping -c 1 8.8.8.8 # Try once.
+    rc=$?
+    if [[ $rc -eq 0 ]]; then
+        ((count = 1)) # If okay, flag loop exit.
+    else
+        sleep 1 # Minimise network storm.
+    fi
+    ((count = count - 1)) # So we don't go forever.
+done
+
+if [[ $rc -eq 0 ]]; then # Make final determination.
+    echo "Finnished"
+    cd /
+    cd /home/pi/pi-wol
+    sudo python3 mqtt_daemon.py
+    cd /
+
+else
+    echo "No Internet connection. Aborting"
+fi
